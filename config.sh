ae5d5c60453bc1f65f0defa93416dd92e136ca12
dmitrysarov <dmvish@yandex.ru>
2017-12-22 01:54:59 +0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -2,6 +2,7 @@
 
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 	curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
 git clone --recursive https://github.com/davidhalter/jedi-vim.git ~/.vim/bundle/jedi-vim
 
-
+git clone git://github.com/Lokaltog/vim-powerline.git ~/.vim/bundle/vim-powerline
