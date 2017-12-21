1b15d4d1ff567ef6bf3f1cb4125e88cfbc34cdce
dmitrysarov <dmvish@yandex.ru>
2017-12-22 00:51:28 +0300

    Update install_vim.sh

--- install_vim.sh
+++ install_vim.sh
@@ -7,7 +7,7 @@ cd vim
             --enable-multibyte \
             --enable-rubyinterp=yes \
             --enable-pythoninterp=yes \
-            --with-python-config-dir=/usr/lib/python2.7/config \
+           # --with-python-config-dir=/usr/lib/python2.7/config \
             --enable-python3interp=yes \
             --with-python3-config-dir=/usr/lib/python3.5/config \
             --enable-perlinterp=yes \
