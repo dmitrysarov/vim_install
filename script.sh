a30d496dafc4f3ef4f282c99484f5cf51bfda717
d.shaulsky <d.shaulsky@millionagents.com>
2017-12-21 18:28:58 +0300

    pathoget + jedi

---
+++ script.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+	curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+echo 'execute pathogen#infect()' >  ~/.vimrc
+echo 'syntax on' >>  ~/.vimrc
+echo 'filetype plugin indent on' >>  ~/.vimrc
+git clone --recursive https://github.com/davidhalter/jedi-vim.git ~/.vim/bundle/jedi-vim
+
+
