d4c1bac9d79302a2a4aeda5ab5b0ffd48bfdf02b
Shauslkii <d.shaulsky@millionagents.com>
2017-12-22 14:30:43 +0300

    new vimrc

--- install_vim.sh
+++ install_vim.sh
@@ -1,5 +1,5 @@
 sudo apt-get install liblua5.1-dev luajit libluajit-5.1 python-dev ruby-dev libperl-dev libncurses5-dev libatk1.0-dev libx11-dev libxpm-dev libxt-dev
-
+sudo apt-get install libncurses5-dev libncursesw5-dev
 git clone https://github.com/vim/vim.git $HOME/vim
 cd $HOME/vim/src
 ./configure --with-features=huge \
