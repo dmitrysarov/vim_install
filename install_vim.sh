f2bf32eb14fafc43d82c610e73515f9f3ac4cd04
dmitrysarov <dmvish@yandex.ru>
2017-12-22 15:58:50 +0000

    fixes of installetions

--- install_vim.sh
+++ install_vim.sh
@@ -1,9 +1,13 @@
-sudo apt-get install liblua5.1-dev luajit libluajit-5.1 python-dev ruby-dev libperl-dev libncurses5-dev libatk1.0-dev libx11-dev libxpm-dev libxt-dev
-sudo apt-get install libncurses5-dev libncursesw5-dev
+apt-get install liblua5.1-dev luajit libluajit-5.1 python-dev ruby-dev libperl-dev libncurses5-dev libatk1.0-dev libx11-dev libxpm-dev libxt-dev -y
+apt-get install libncurses5-dev libncursesw5-dev -y
 git clone https://github.com/vim/vim.git $HOME/vim
 cd $HOME/vim/src
 ./configure --with-features=huge \
             --enable-pythoninterp=yes \
             --prefix=/usr/local
 make && make install
-echo alias vim= '/usr/local/bin/vim/' >> ~/.bashrc
+mkdir -p $HOME/bin
+cd $HOME/bin
+ln -s /usr/local/bin/vim
+which vim
+vim --version
