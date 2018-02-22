586dd839a9735a0b4cb9e21e52330bbdd668d712
dmitrysarov <d.shaulsky@millionagents.com>
2018-02-22 16:24:30 +0300

    fix path

--- config.sh
+++ config.sh
@@ -7,7 +7,7 @@ git clone --recursive https://github.com/davidhalter/jedi-vim.git ~/.vim/bundle/
 
 git clone git://github.com/Lokaltog/vim-powerline.git ~/.vim/bundle/vim-powerline
 
-git clone https://github.com/vim-python/python-syntax.git ~/.vim/bundle
+git clone https://github.com/vim-python/python-syntax.git ~/.vim/bundle/python-syntax
 
 git clone https://github.com/ctrlpvim/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim
    
